d6a996b7061262cc1dd4295ac3bb88ea56e4bdea
sunjom <saysaysay223@naver.com>
2022-03-30 09:44:59 +0900

    ㅂㅈㄷ

--- hi.java
+++ hi.java
@@ -2,5 +2,6 @@ public class hi{
     public static void main(String[] args){
         System.out.println("hello");
         System.out.println("찌이발!");
+        System.out.println("qweqwe");
     }
 }
