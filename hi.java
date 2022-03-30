31caa7f9b45c45be7658561ec3ac377a40b88dbd
sunjom <saysaysay223@naver.com>
2022-03-30 09:45:05 +0900

    Revert "ㅂㅈㄷ"
    This reverts commit d6a996b7061262cc1dd4295ac3bb88ea56e4bdea.

--- hi.java
+++ hi.java
@@ -2,6 +2,5 @@ public class hi{
     public static void main(String[] args){
         System.out.println("hello");
         System.out.println("찌이발!");
-        System.out.println("qweqwe");
     }
 }
